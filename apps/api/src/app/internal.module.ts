d796fcf45bc672ff269b3fafe57b08b892576f6a
Julio Sansossio <juliosansossio@gmail.com>
2022-11-05 22:40:00 -0400

    Server static files and integrate react on nestjs output

--- apps/api/src/app/internal.module.ts
+++ apps/api/src/app/internal.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { GithubModule } from "@github-commits-history/github";
+import { ServeStaticModule } from '@nestjs/serve-static';
 import config from "../config";
+import { join } from "path";
 
 @Module({
   imports: [
@@ -9,14 +11,17 @@ import config from "../config";
       envFilePath: './apps/api/.env',
       load: config
     }),
-
     GithubModule.registerAsync({
       imports: [ConfigModule.forRoot()],
       useFactory: (config: ConfigService) => ({
         baseUrl: config.get<string>('github.baseUrl')
       }),
       inject: [ConfigService]
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'public')
     })
   ]
 })
 export class InternalModule {}
+console.log(join(__dirname, 'public'))
